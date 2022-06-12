c0d4825f318ec4eb456253a3d97fb8651bc06925
senera <smallikarachchi@kaleris.com>
2022-06-12 10:44:58 +0530

    simple color

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", ()=>{
+
+    //get random number between 0 - 3
+    const randomNumber = this.getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+
+}); 
+
+getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length)
+}
