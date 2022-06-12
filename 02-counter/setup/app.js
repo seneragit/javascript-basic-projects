4ed0914d1c0749fbd1c0d34162f718353c57c315
senera <smallikarachchi@kaleris.com>
2022-06-12 12:06:54 +0530

    counter

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,33 @@
+// set initial count
+let count = 0;
+
+// select valu and button
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+        const styles = event.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = 'green'
+        }
+        if (count < 0) {
+            value.style.color = 'red'
+        }
+        if (count === 0) {
+            value.style.color = '#222'
+        }
+        value.textContent = count;
+    })
+})
+
+
